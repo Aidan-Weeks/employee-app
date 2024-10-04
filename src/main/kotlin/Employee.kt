e405c903aac561fd2b7a4a0f4e1cf2b8a3bb2035
Aidan Weeks <aidanweeks1066@gmail.com>
2024-10-04 15:19:38 +0100

    finished adding components

--- src/main/kotlin/Employee.kt
+++ src/main/kotlin/Employee.kt
@@ -2,4 +2,39 @@ package ie.setu
 
 import jdk.jfr.Percentage
 
-class Employee(var firstName: String, var surname: String, var gender: Char, var employeeID: Int, var grossSalary: Double, var payePercentage: Double, var prsiPercentage: Double, var annualBonus: Double, var cycleToWorkMonthlyDeduction: Double)
+class Employee (var firstName: String, var surname : String, var gender : Char,
+                var employeeID : Int, var grossSalary : Double, var payePercentage : Double,
+                var prsiPercentage : Double, var annualBonus : Double,
+                var cycleToWorkMonthlyDeduction : Double, ){
+
+
+    fun getPayslip() = """                       
+    |_______________________________________________________________________                                                                 
+    |     Monthly Payslip:             ${getFullName()}, ID: ${employeeID}                                                                                 
+    |_______________________________________________________________________                                                                 
+    |     PAYMENT DETAILS (gross pay: ${getGrossMonthlyPay()})     
+    |          Salary: ${getMonthlySalary()}
+    |          Bonus:  ${getMonthlyBonus()}
+    |_______________________________________________________________________
+    |     DEDUCTION DETAILS (total Deductions: ${getTotalMonthlyDeductions()})    
+    |          PAYE: ${getMonthlyPAYE()}
+    |          PRSI: ${getMonthlyPRSI()}
+    |          Cycle To Work: ${cycleToWorkMonthlyDeduction}
+    |_______________________________________________________________________
+    |    NET PAY:${getNetMonthlyPay()}
+    |_______________________________________________________________________""".trimMargin("|")
+
+    fun getFullName() = when (gender){
+        'm', 'M' -> "Mr. ${firstName} ${surname}"
+        'f', 'F' -> "Ms.  ${firstName} ${surname}"
+        else ->  "${firstName} ${surname}"
+    }
+
+    fun getMonthlySalary() = roundTwoDecimals(grossSalary / 12)
+    fun getMonthlyPRSI() = roundTwoDecimals(getMonthlySalary() * (prsiPercentage / 100))
+    fun getMonthlyPAYE() = roundTwoDecimals(getMonthlySalary() * (payePercentage / 100))
+    fun getMonthlyBonus() = roundTwoDecimals((annualBonus / 12))
+    fun getGrossMonthlyPay() = roundTwoDecimals(getMonthlySalary() + (annualBonus / 12))
+    fun getTotalMonthlyDeductions() = roundTwoDecimals((getMonthlyPRSI() + getMonthlyPAYE() + cycleToWorkMonthlyDeduction))
+    fun getNetMonthlyPay() = roundTwoDecimals(getGrossMonthlyPay() - getTotalMonthlyDeductions())
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,105 @@
 package ie.setu
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import ie.setu.EmployeeAPI
+import ie.setu.Employee
+import mu.KotlinLogging
+import kotlin.math.round
+
+var employees = EmployeeAPI()
+val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>){
+    logger.info { "Launching Employee App" }
+    start()
+}
+
+fun menu() : Int {
+    print(""" 
+         |Employee Menu
+         |   1. Add Employee
+         |   2. List All Employees
+         |   3. Search Employees 
+         |   4. Print Payslip for Employee
+         |  -1. Exit
+         |       
+         |Enter Option : """.trimMargin())
+    return readln().toInt()
+}
+
+fun start() {
+    var input: Int
+
+    do {
+        input = menu()
+        when (input) {
+            1 -> add()
+            2 -> list()
+            3 -> search()
+            4 -> paySlip()
+            -99 -> dummyData()
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+}
+
+fun list(){
+    logger.info { "Listing Employees" }
+    employees.findAll()
+        .forEach{ println(it) }
+}
+
+fun search() {
+    logger.info { "Searching Employees" }
+    val employee = getEmployeeById()
+    if (employee == null)
+        println("No employee found")
+    else
+        println(employee)
+}
+
+internal fun getEmployeeById(): Employee? {
+    print("Enter the employee id to search by: ")
+    val employeeID = readLine()!!.toInt()
+    return employees.findOne(employeeID)
+}
+
+fun paySlip(){
+    val employee = getEmployeeById()
+    if (employee != null)
+        println(employee.getPayslip())
+}
+
+fun dummyData() {
+    employees.create(Employee("Joe", "Soap", 'm', 0, 35655.43, 31.0, 7.5, 2000.0, 25.6))
+    employees.create(Employee("Joan", "Murphy", 'f', 0, 54255.13, 32.5, 7.0, 1500.0, 55.3))
+    employees.create(Employee("Mary", "Quinn", 'f', 0, 75685.41, 40.0, 8.5, 4500.0, 0.0))
+}
+
+fun add(){
+    logger.info { "Adding an Employee" }
+    print("Enter first name: ")
+    val firstName = readlnOrNull().toString()
+    print("Enter surname: ")
+    val surname = readlnOrNull().toString()
+    print("Enter gender (m/f): ")
+    val gender = readln().toCharArray()[0]
+    print("Enter gross salary: ")
+    val grossSalary = readln().toDouble()
+    print("Enter PAYE %: ")
+    val payePercentage = readln().toDouble()
+    print("Enter PRSI %: ")
+    val prsiPercentage = readln().toDouble()
+    print("Enter Annual Bonus: ")
+    val annualBonus= readln().toDouble()
+    print("Enter Cycle to Work Deduction: ")
+    val cycleToWorkMonthlyDeduction= readln().toDouble()
+
+    employees.create(Employee(firstName, surname, gender, 0, grossSalary, payePercentage, prsiPercentage, annualBonus, cycleToWorkMonthlyDeduction))
+}
+
+//https://discuss.kotlinlang.org/t/how-do-you-round-a-number-to-n-decimal-places/8843
+//There are several options...try each of them out
+fun roundTwoDecimals(number: Double) = round(number * 100) / 100
+//fun roundTwoDecimals(number: Double) = "%.2f".format(number).toDouble()
